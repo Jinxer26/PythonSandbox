b6c933571a73ee8e114805dabb0f6a04eccebe9a
Nithin Raj <pro.nithin.raj@gmail.com>
2024-08-30 12:06:39 +0530

    Added PythonClasses.py

---
+++ pythonclasses.py
@@ -0,0 +1,22 @@
+class Person:
+	def __init__(self,fname: str,lname: str):
+		self.firstname = fname 
+		self.lastname = lname 
+
+	def printname(self):
+		return f'Firstname : {self.firstname}, Lastname : {self.lastname}'
+
+	def __str__(self):
+		pass
+
+class Student(Person):
+
+	def __init__(self,fname: str,lname: str):
+		Person.__init__(self,fname,lname)
+	
+	def __str__(self):
+		return f'Welcome to Class {self.firstname} {self.lastname}'
+
+
+p1 = Student("Nithin","Raj")
+print(p1.printname())
